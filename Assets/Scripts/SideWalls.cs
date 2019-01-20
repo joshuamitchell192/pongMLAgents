ca2dd92a10c74464900a16d744aac469119565cf
josh2397 <josh23797@gmail.com>
2019-01-20 13:42:09 +1000

    Initial commit
    Pong environment has been created, now using mlagents

---
+++ Assets/Scripts/SideWalls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D hitInfo) {
+		if (hitInfo.name == "Ball"){
+			string wallName = transform.name;
+			Debug.Log(wallName);
+			GameManager.Score(wallName);
+			hitInfo.gameObject.SendMessage("RestartGame", 1.0f, SendMessageOptions.RequireReceiver);
+		}
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ballControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public Camera cam;
+    public Color BlackColour = Color.black;
+    public Color GreenColour = Color.green;
+    public Color RedColour = Color.red;
+    public bool win = false;
+    void GoBall() {
+        float rand = Random.Range(0, 2);
+        if (rand < 1){
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    } 
+
+    void ResetBall() {
+        rb2d.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+    
+    void RestartGame() {
+        ResetBall();
+        Invoke("GoBall", 1);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.collider.CompareTag("Player")) {
+            Vector2 vel; 
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y /2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (win == true ) {
+            cam = GetComponent<Camera>();
+            cam.backgroundColor = Color.Lerp(BlackColour, GreenColour, 2.0f);
+            cam.backgroundColor = Color.Lerp(GreenColour, BlackColour, 2.0f);
+            win = false;
+        }
+    }
+}
